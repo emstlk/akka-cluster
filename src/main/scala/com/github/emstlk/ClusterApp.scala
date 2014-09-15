d58b7aab36df322857536a9b0cfae5c8f11731f3
emaster <a.ermoshkin@nexters.com>
2014-09-15 13:39:29 +0400

    Fixed config for test on 2 nodes

--- src/main/scala/com/github/emstlk/ClusterApp.scala
+++ src/main/scala/com/github/emstlk/ClusterApp.scala
@@ -7,23 +7,37 @@ import com.github.emstlk.UserManager.UpdateUserCoins
 import scala.util.Random
 
 object ClusterApp {
+
   def main(args: Array[String]): Unit = {
-    val port = if (args.isEmpty) 2555 else args(0).toInt
+    if (args.isEmpty || args.length != 2) {
+      println("usage: ./sbt \"run <seed_address>[:port] <own_address>[:port]\"")
+    } else {
+      val (seedAddr, seedPort) = parseAddr(args(0))
+      val (ownAddr, ownPort) = parseAddr(args(1))
 
-    val config = ConfigFactory.parseString( s"""
-      akka.remote.netty.tcp.port="$port"
+      val config = ConfigFactory.parseString( s"""
+      akka.remote.netty.tcp.hostname="$ownAddr"
+      akka.remote.netty.tcp.port="$ownPort"
+      akka.cluster.seed-nodes=["akka.tcp://ClusterApp@$seedAddr:$seedPort"]
       """).withFallback(ConfigFactory.load)
-    val system = ActorSystem("app", config)
+      val system = ActorSystem("ClusterApp", config)
 
-    val userManager = system.actorOf(UserManager.props(), "userManager")
+      val userManager = system.actorOf(UserManager.props(), "userManager")
 
-    if (port != 2555) {
-      Thread.sleep(3000)
+      if (seedAddr != ownAddr || seedPort != ownPort) {
+        Thread.sleep(3000)
 
-      1 to 5 foreach { uid =>
-        userManager ! UpdateUserCoins(uid, Random.nextInt(100))
-        Thread.sleep(200)
+        1 to 5 foreach { uid =>
+          userManager ! UpdateUserCoins(uid, Random.nextInt(100))
+          Thread.sleep(200)
+        }
       }
     }
+  }
+
+  def parseAddr(addr: String) = {
+    val splitted = addr.split(':')
+    if (splitted.length == 2) (splitted(0), splitted(1))
+    else (splitted(0), "2552")
   }
 }
